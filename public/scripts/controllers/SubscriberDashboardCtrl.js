e7991aefbbdb2390a2a7444690e09fbc1a449df0
Aodhan Hayter <aodhan.hayter@gmail.com>
2015-06-18 09:48:43 -0600

    Subscriber dashboard can now edit profile info and individual listings on the front end

--- public/scripts/controllers/SubscriberDashboardCtrl.js
+++ public/scripts/controllers/SubscriberDashboardCtrl.js
@@ -2,6 +2,7 @@ var app = angular.module('lightRail');
 
 app.controller('SubscriberDashboardCtrl', function($scope, subData, GeneralUserService) {
 
+	//Subscriber
 	$scope.subscriber = subData;
 	console.log($scope.subscriber);
 
@@ -15,8 +16,30 @@ app.controller('SubscriberDashboardCtrl', function($scope, subData, GeneralUserS
 		zip: $scope.subscriber.contact_address.zip_code
 	};
 
+	//listings
 	$scope.listing = GeneralUserService.apartmentData[0];
-	console.log('Listing:', GeneralUserService.apartmentData[0])
+	console.log($scope.listing);
+
+	$scope.editListing = {
+		listingName: $scope.listing.apartment_name,
+		location: {
+			street: $scope.listing.location.street_address,
+			city: $scope.listing.location.city,
+			state: {
+				name: $scope.listing.location.state
+			},
+			zip: $scope.listing.location.zip_code
+		},
+		phone: $scope.listing.phone,
+		webpage: $scope.listing.webpage,
+		rentRange: {
+			max: $scope.listing.rent_range.max,
+			min: $scope.listing.rent_range.min
+		},
+		coverPicture: $scope.listing.cover_picture,
+		picturesArray: $scope.listing.picturesArray,
+		description: $scope.listing.description
+	};
 
 	$scope.states = [
 		{name: 'AZ'},
@@ -26,7 +49,30 @@ app.controller('SubscriberDashboardCtrl', function($scope, subData, GeneralUserS
 	];
 
 	$scope.selectListing = function(apartment) {
-		return $scope.listing = apartment;
+		$scope.listing = apartment;
+
+		$scope.editListing = {
+		listingName: $scope.listing.apartment_name,
+		location: {
+			street: $scope.listing.location.street_address,
+			city: $scope.listing.location.city,
+			state: {
+				name: $scope.listing.location.state
+			},
+			zip: $scope.listing.location.zip_code
+		},
+		phone: $scope.listing.phone,
+		webpage: $scope.listing.webpage,
+		rentRange: {
+			max: $scope.listing.rent_range.max,
+			min: $scope.listing.rent_range.min
+		},
+		coverPicture: $scope.listing.cover_picture,
+		picturesArray: $scope.listing.picturesArray,
+		description: $scope.listing.description
+	};
+
+		return $scope.listing
 	};
 
 	$scope.saveProfile = function(companyName, streetAddress, city, state, zip) {
@@ -38,6 +84,49 @@ app.controller('SubscriberDashboardCtrl', function($scope, subData, GeneralUserS
 		$scope.subscriber.contact_address.zip_code = zip;
 
 		return $scope.subscriber;
+	};
+
+	$scope.saveListing = function(listingName, location, phone, webpage, rentRange, picturesArray, description) {
+		$scope.listing.apartment_name = listingName;
+		$scope.listing.location = {
+			street_address: location.street,
+			city: location.city,
+			state: location.state,
+			zip_code: location.zip
+		};
+		$scope.listing.phone = phone;
+		$scope.listing.webpage = webpage;
+		$scope.listing.rent_range = {
+			max: rentRange.max,
+			min: rentRange.min
+		};
+		$scope.listing.picturesArray = picturesArray;
+		$scope.listing.description = description;
+
+		$scope.editListing = {
+			listingName: $scope.listing.apartment_name,
+			location: {
+				street: $scope.listing.location.street_address,
+				city: $scope.listing.location.city,
+				state: {
+					name: $scope.listing.location.state
+				},
+				zip: $scope.listing.location.zip_code
+			},
+			phone: $scope.listing.phone,
+			webpage: $scope.listing.webpage,
+			rentRange: {
+				max: $scope.listing.rent_range.max,
+				min: $scope.listing.rent_range.min
+			},
+			coverPicture: $scope.listing.cover_picture,
+			picturesArray: $scope.listing.picturesArray,
+			description: $scope.listing.description
+		};
+
+		console.log($scope.editListing);
+
+		return $scope.listing;
 	};
 
 	//seed data for development only
