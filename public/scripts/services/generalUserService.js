6d1985f826bda2451b8f3235f2fb49291ad0578b
Rick Cid <rick@hispand.com>
2015-06-19 13:08:28 -0600

    Added changes to newApartmentListingTmpl

--- public/scripts/services/generalUserService.js
+++ public/scripts/services/generalUserService.js
@@ -44,36 +44,8 @@ app.service('GeneralUserService', function($http, $q) {
     }
     return allItems.replace(',', ' ');
   };
-
-  this.addApartmentListing = function(apartment) {
-    var deferred = q.defer();
-    $http({
-      method: 'POST',
-      url: '/api/subscriber/addApartmentListing',
-      data: {
-        name: apartment.name,
-        street_address: apartment.street_address,
-        city: apartment.city,
-        state: apartment.state,
-        zip_code: apartment.zip_code,
-        phone_number: apartment.phone_number,
-        webpage: apartment.webpage,
-        description: apartment.description,
-        photos: apartment.photos,
-        units: apartment.units,
-        amenities: apartment.amenities,
-        amenities_additional: apartment.amenities_additional
-      }
-    }).then(function(response) {
-      deferred.resolve(response.data);
-    }, function(error) {
-      deferred.reject(error);
-    });
-    return deferred.promise;
-  };
-
   
-
+  
   //*** SEED DATA FOR MAIN PAGE ***//
 
 
