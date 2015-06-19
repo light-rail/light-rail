a375b7d4b79679ca74f97c0e7f5d6dc32b468b3d
Rick Cid <rick@hispand.com>
2015-06-18 23:53:07 -0600

    Added SubscriberCtrl and tied to service

--- public/scripts/controllers/SubscriberCtrl.js
+++ public/scripts/controllers/SubscriberCtrl.js
@@ -3,4 +3,25 @@ var app = angular.module('lightRail');
 app.controller('SubscriberCtrl', function($scope, $location, GeneralUserService) {
 
 
+
+  //** Add Unit Modal Content **/
+  $scope.addUnit = function(unit) {
+    var newUnit = '';
+
+    newUnit = {
+      bedrooms: unit.bedrooms,
+      bathrooms: unit.bathrooms,
+      square_feet: unit.square_feet,
+      monthly_rent: unit.monthly_rent
+    }
+    console.log('newUnit2', newUnit)
+
+    $scope.newUnit
+  }
+
+
+  $scope.addApartmentListing = function(apartment) {
+
+  }
+
 });
--- public/scripts/services/generalUserService.js
+++ public/scripts/services/generalUserService.js
@@ -37,7 +37,6 @@ app.service('GeneralUserService', function($http, $q) {
   };
 
 
-
   this.displayAll = function(obj) {
     var allItems = '';
     for (var prop in obj) {
@@ -46,6 +45,32 @@ app.service('GeneralUserService', function($http, $q) {
     return allItems.replace(',', ' ');
   };
 
+  this.addApartmentListing = function(apartment) {
+    var deferred = q.defer();
+    $http({
+      method: 'POST',
+      url: '/api/subscriber/addApartmentListing',
+      data: {
+        name: apartment.name,
+        street_address: apartment.street_address,
+        city: apartment.city,
+        state: apartment.state,
+        zip_code: apartment.zip_code,
+        phone_number: apartment.phone_number,
+        webpage: apartment.webpage,
+        description: apartment.description,
+        photos: apartment.photos,
+        units: apartment.units,
+        amenities: apartment.amenities,
+        amenities_additional: apartment.amenities_additional
+      }
+    }).then(function(response) {
+      deferred.resolve(response.data);
+    }, function(error) {
+      deferred.reject(error);
+    });
+    return deferred.promise;
+  };
 
   
 
