c608aa4ea7c83d5c7aa5da3c961a31248ff5e9da
Rick Cid <rick@hispand.com>
2015-06-22 10:03:45 -0600

    Adjusted flow for Subscriber listing registration

--- public/scripts/controllers/SubscriberCtrl.js
+++ public/scripts/controllers/SubscriberCtrl.js
@@ -1,23 +1,54 @@
 var app = angular.module('lightRail');
 
-app.controller('SubscriberCtrl', function($scope, $location, SubscriberService) {
- $scope.apartment = {
-  units:[]
- };
+app.controller('SubscriberCtrl', function($scope, $location, $routeParams, SubscriberService, GeneralUserService) {
 
- $scope.unit = {};
+  //Used to link seeded Data -- Erase when linked to Subscriber backend
+  var selectedSubscriberData = GeneralUserService.apartmentData[$routeParams.subscriberId];
+  
+  $scope.seededApartment = selectedSubscriberData;
+  //** End of Seeded Data Reference
+
+  $scope.apartment = {
+    units:[],
+    pictures_array: []
+  };
+
+  $scope.unit = {};
+  $scope.picture = {};
+
 
   //** Add Unit Modal Content **/
   $scope.addUnit = function(unit) {
     $scope.apartment.units.push(unit);
     $scope.unit = {};
+    console.log('apartment', $scope.apartment)
+    console.log('unit', $scope.unit)
   };
 
+  //** Add Pictures Container Content **/
+  $scope.addPicture = function(picture) {
+    $scope.apartment.pictures_array.push(picture);
+    $scope.picture = {};
+    console.log('picture', $scope.picture)
+    console.log('apartment', $scope.apartment)
+  };
+
+  //** Address Verification
+  $scope.verifyApartmentAddress = function(address) {
+    SubscriberService.verifyApartmentAddress(address);
+  };
 
   //** Submits Listing to Service **/
   $scope.addApartmentListing = function(apartment) {
-
-    SubscriberService.addApartmentListing(apartment);
-  }
+    SubscriberService.addApartmentListing(apartment).then (function(res) {
+      toaster.pop('success', 'Your address registration was successfull!');
+      $location.path('/subscriber/new-listing/:subscriberId');//Needs a :subscriberId
+    }, function(err) {
+      console.log('AddApartment err', err)
+      //Need to add an error.status for address not being within range of lightrail
+      if(err.status === 11000) toaster.pop('error', 'This listing is already registered');
+      else toaster.pop('error', 'Sorry, something went wrong!');
+    });
+  };
 
 });
--- public/scripts/services/subscriberService.js
+++ public/scripts/services/subscriberService.js
@@ -3,6 +3,21 @@ var app = angular.module('lightRail');
 
 app.service('SubscriberService', function($http, $q) { 
 
+  this.verifyApartmentaddress = function(address) {
+    var deferred = $q.defer();
+    console.log('subscriberServiceAddress', address)
+    $http({
+      method: 'POST',
+      url: '/api/subscriber/verifyApartmentAddress',
+      data: address
+    }).then(function(response) {
+      deferred.resolve(response.data);
+    }, function(error) {
+      deferred.reject(error);
+    });
+    return deferred.promise;
+  };
+
 
 
   this.addApartmentListing = function(apartment) {
@@ -19,6 +34,8 @@ app.service('SubscriberService', function($http, $q) {
     });
     return deferred.promise;
   };
+
+  // this.addApartmentPicture = function()
 
 
 });
